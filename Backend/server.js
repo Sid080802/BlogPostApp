91a22554c427938a834b7601df875924eb8cb021
Siddharth Godambe <siddarthgodambe123@gmail.com>
2025-02-03 14:45:31 +0530

    file update

--- Backend/server.js
+++ Backend/server.js
@@ -133,3 +133,4 @@ app.delete("/api/posts/:id", async (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+module.exports = app;
